8bb3f9c32500446d4b75cc72af60fc3635144fec
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-23 20:35:29 +0300

    Initial Commit

---
+++ CrazyCircleApp/CrazyCircleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyCircleAppApp.swift
+//  CrazyCircleApp
+//
+//  Created by jopootrivatel on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrazyCircleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
